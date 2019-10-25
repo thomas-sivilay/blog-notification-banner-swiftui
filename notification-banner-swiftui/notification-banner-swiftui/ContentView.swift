3e91c4468cb423ed7da20c1da6ad64a6361d742f
Thomas SIVILAY <thomas.sivilay@gmail.com>
2019-10-25 18:59:40 +1100

    Initial commit

---
+++ notification-banner-swiftui/notification-banner-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  notification-banner-swiftui
+//
+//  Created by thomas on 25/10/19.
+//  Copyright © 2019 thomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
