b2299577669fcca03fe9e284e4ccf381fbac9cd8
Thomas SIVILAY <thomas.sivilay@gmail.com>
2019-11-04 18:54:23 +1100

    Added button banner to show that we can have callback

--- notification-banner-swiftui/notification-banner-swiftui/BannerView.swift
+++ notification-banner-swiftui/notification-banner-swiftui/BannerView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct BannerData {
     let title: String
     let subtitle: String? = nil
+    var actionTitle: String? = nil
     var level: Level = .info
     
     let style: Style
@@ -18,6 +19,7 @@ struct BannerData {
     enum Style {
         case fullWidth
         case popUp
+        case action
     }
     
     enum Level {
@@ -38,12 +40,14 @@ struct BannerData {
 }
 
 extension BannerData {
-    func makeBanner() -> some View {
+    func makeBanner(action: (() -> Void)? = nil) -> some View {
         switch style {
         case .popUp:
             return AnyView(LocalNotification(data: self))
         case .fullWidth:
             return AnyView(FullWidthBanner(data: self))
+        case .action:
+            return AnyView(BannerWithButton(data: self, action: action))
         }
     }
 }
@@ -51,11 +55,12 @@ extension BannerData {
 struct BannerView: ViewModifier {
     @Binding var isPresented: Bool
     let data: BannerData
+    let action: (() -> Void)?
     
     func body(content: Content) -> some View {
         ZStack(alignment: .top) {
             if isPresented {
-                data.makeBanner()
+                data.makeBanner(action: action)
                     .animation(.easeInOut)
                     .transition(AnyTransition.move(edge: .top).combined(with: .opacity))
                     .zIndex(100)
@@ -71,8 +76,8 @@ struct BannerView: ViewModifier {
 }
 
 extension View {
-    func banner(isPresented: Binding<Bool>, data: BannerData) -> some View {
-        self.modifier(BannerView(isPresented: isPresented, data: data))
+    func banner(isPresented: Binding<Bool>, data: BannerData, action: (() -> Void)? = nil) -> some View {
+        self.modifier(BannerView(isPresented: isPresented, data: data, action: action))
     }
 }
 
@@ -116,4 +121,27 @@ struct LocalNotification: View {
         .shadow(color: Color.black.opacity(0.1), radius: 8, x: 0, y: 8)
         .padding()
     }
+}
+
+struct BannerWithButton: View {
+    let data: BannerData
+    var action: (() -> Void)?
+    
+    var body: some View {
+        HStack {
+            Text(data.title)
+            Spacer()
+            Button(action: {
+                self.action?()
+            }) {
+                Text(data.actionTitle ?? "Action")
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12))
+                    .background(Rectangle().foregroundColor(Color.black.opacity(0.3)))
+            }
+        }
+        .padding(EdgeInsets(top: 12, leading: 8, bottom: 12, trailing: 8))
+        .background(data.level.tintColor)
+    }
+
 }
--- notification-banner-swiftui/notification-banner-swiftui/ContentView.swift
+++ notification-banner-swiftui/notification-banner-swiftui/ContentView.swift
@@ -10,25 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var hideAllBanners: Bool = false
     @State private var showBanner: Bool = false
     @State private var tappedIndex: Int = 0
+    @State private var countActionTap: Int = 0
     
     var body: some View {
         NavigationView {
             ScrollView {
+                VStack {
+                    Text("Example of banners").font(.headline)
+                    Text("Tap one of them to show banner in this scroll view").font(.subheadline)
+                }.padding()
+                Divider()
                 ForEach(0..<allBanners.count) { index in
                     allBanners[index]
                         .makeBanner()
                         .onTapGesture {
                             self.tappedIndex = index
                             self.showBanner = true
-                    }
+                        }
                 }
+                .padding()
+                Divider()
+                Text("Count tapped on action banner: \(countActionTap)")
             }
             .animation(.easeInOut)
             .transition(.move(edge: .top))
             .navigationBarTitle("Demo", displayMode: .inline) // large title is always tricky
-            .banner(isPresented: $showBanner, data: allBanners[tappedIndex])
+            .banner(isPresented: $showBanner, data: allBanners[tappedIndex], action: {
+                self.countActionTap += 1
+            })
         }
     }
     
@@ -40,15 +52,14 @@ struct ContentView: View {
 }
 
 var allBanners = [
-    BannerData(title: "Info", level: .info, style: .fullWidth),
-    BannerData(title: "Success", level: .success, style: .fullWidth),
-    BannerData(title: "Warning", level: .warning, style: .fullWidth),
-    BannerData(title: "Error", level: .error, style: .fullWidth),
+    BannerData(title: "Success tap to dismiss", level: .success, style: .fullWidth),
+    BannerData(title: "Warning tap to dismiss", level: .warning, style: .fullWidth),
+    BannerData(title: "Error tap to dismiss", level: .error, style: .fullWidth),
     
     BannerData(title: "Info", level: .info, style: .popUp),
-    BannerData(title: "Success", level: .success, style: .popUp),
-    BannerData(title: "Warning", level: .warning, style: .popUp),
-    BannerData(title: "Error", level: .error, style: .popUp),
+    
+    BannerData(title: "Banner with action", actionTitle: "Increment", level: .success, style: .action),
+    BannerData(title: "Banner with action", actionTitle: "Decrement", level: .error, style: .action)
 ]
 
 struct ContentView_Previews: PreviewProvider {
